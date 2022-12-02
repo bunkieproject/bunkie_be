8db1532134fb290bc3eced42ed3cee8c30fd4fe4
Yunus Emre Karaoğlan <karaoglan18@itu.edu.tr>
2022-12-03 00:55:29 +0300

    Add UpdateHouseAd() function

--- controllers/adController.go
+++ controllers/adController.go
@@ -98,3 +98,86 @@ func DeleteHouseAd() gin.HandlerFunc {
 		}
 	}
 }
+
+func UpdateHouseAd() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var houseAd models.UpdatedHouseAd
+		var foundAd models.HouseAd
+
+		// Bind JSON to struct
+		if err := c.BindJSON(&houseAd); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := houseAdCollection.FindOne(ctx, bson.M{"user_id": houseAd.User_id}).Decode(&foundAd)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if houseAd.Header == nil {
+			houseAd.Header = foundAd.Header
+		}
+		if houseAd.Description == nil {
+			houseAd.Description = foundAd.Description
+		}
+
+		// Validate the input
+		validationErr := validate.Struct(houseAd)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		update_map := setUpdateMap(houseAd)
+
+		update := bson.M{"$set": update_map}
+		err = houseAdCollection.FindOneAndUpdate(ctx, bson.M{"user_id": houseAd.User_id}, update).Decode(&foundAd)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, foundAd)
+
+	}
+}
+
+func setUpdateMap(ad models.UpdatedHouseAd) bson.M {
+	update_map := bson.M{}
+	if ad.Header != nil {
+		update_map["header"] = ad.Header
+	}
+	if ad.Description != nil {
+		update_map["description"] = ad.Description
+	}
+	if ad.School != nil {
+		update_map["school"] = ad.School
+	}
+	if ad.City != nil {
+		update_map["city"] = ad.City
+	}
+	if ad.District != nil {
+		update_map["district"] = ad.District
+	}
+	if ad.Neighborhood != nil {
+		update_map["neighborhood"] = ad.Neighborhood
+	}
+	if ad.Price != nil {
+		update_map["price"] = ad.Price
+	}
+	if ad.Size != nil {
+		update_map["size"] = ad.Size
+	}
+	if ad.Number_of_rooms != nil {
+		update_map["number_of_rooms"] = ad.Number_of_rooms
+	}
+	if ad.House_photo != nil {
+		update_map["house_photo"] = ad.House_photo
+	}
+	return update_map
+}
