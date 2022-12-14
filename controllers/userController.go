21dc15dd95e46a3d38c695d8d5f6c53feee64e72
Yunus Emre Karaoğlan <karaoglan18@itu.edu.tr>
2022-12-14 12:14:56 +0300

    Add DeleteUser

--- controllers/userController.go
+++ controllers/userController.go
@@ -357,24 +357,95 @@ func DeleteAccount() gin.HandlerFunc {
 			return
 		}
 
-		err := userCollection.FindOne(c, bson.M{"user_id": request.User_id}).Decode(&user)
+		err := userCollection.FindOne(c, bson.M{"token": request.Token}).Decode(&user)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
 			return
 		}
 
-		if *user.Token != *request.Token {
+		if user.User_id != request.User_id {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
 			return
 		}
 
-		resultDeleteNumber, err := userCollection.DeleteOne(c, bson.M{"user_id": request.User_id})
+		// delete user from database
+		resultDeleteUser, err := userCollection.DeleteOne(c, bson.M{"token": request.Token})
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"result": resultDeleteNumber})
+		// delete user's posts from database
+		resultDeletePostsBunkie, err := bunkieAdCollection.DeleteMany(c, bson.M{"user_id": request.User_id})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		resultDeletePostsRoom, err := roomAdCollection.DeleteMany(c, bson.M{"user_id": request.User_id})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Account deleted successfully", "resultDeleteUser": resultDeleteUser, "resultDeletePostsBunkie": resultDeletePostsBunkie, "resultDeletePostsRoom": resultDeletePostsRoom})
+	}
+}
+
+func DeleteUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var request models.DeleteUserRequest
+		var user models.AccountInfo
+
+		if err := c.BindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(request)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(c, bson.M{"token": request.Token}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+			return
+		}
+
+		if *user.UserType != "admin" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "You are not admin"})
+			return
+		}
+
+		err = userCollection.FindOne(c, bson.M{"user_id": request.User_id}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+			return
+		}
+
+		// delete user from database
+		resultDeleteUser, err := userCollection.DeleteOne(c, bson.M{"user_id": request.User_id})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// delete user's posts from database
+		resultDeletePostsBunkie, err := bunkieAdCollection.DeleteMany(c, bson.M{"user_id": request.User_id})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		resultDeletePostsRoom, err := roomAdCollection.DeleteMany(c, bson.M{"user_id": request.User_id})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully", "resultDeleteUser": resultDeleteUser, "resultDeletePostsBunkie": resultDeletePostsBunkie, "resultDeletePostsRoom": resultDeletePostsRoom})
 	}
 }
 
