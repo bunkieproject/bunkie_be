f5d6dfb6a52a6442c613b7a8559ecbe80d542261
itu-itis22-karaoglan18 <yunusemrekaraoglan188@gmail.com>
2022-12-24 12:48:05 +0300

    port

--- main.go
+++ main.go
@@ -11,16 +11,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "8080"
-	}
+	port := "8080"
 
 	router := gin.New()
 	router.Use(gin.Logger())
