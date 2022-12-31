f949704a3ff2aa6dcd6c4c93acc9ab0f4d9c06e1
Yunus Emre Karaoğlan <karaoglan18@itu.edu.tr>
2022-12-31 12:34:48 +0300

    DisplayProfile function is seperated into display own or other user's profile

--- controllers/userController.go
+++ controllers/userController.go
@@ -698,83 +698,133 @@ func DisplayProfile() gin.HandlerFunc {
 		}
 
 		var displayBanAndWarn bool
-		if user.UserType != nil {
-			if *user.UserType == "admin" {
-				displayBanAndWarn = true
+		if user.User_id != request.User_id { // other user's profile
+			err = userCollection.FindOne(c, bson.M{"user_id": request.User_id}).Decode(&user)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+				return
+			}
+
+			if user.UserType != nil {
+				if *user.UserType == "admin" {
+					displayBanAndWarn = true
+				} else {
+					displayBanAndWarn = false
+				}
 			} else {
 				displayBanAndWarn = false
 			}
-		} else {
-			displayBanAndWarn = false
-		}
 
-		err = userCollection.FindOne(c, bson.M{"user_id": request.User_id}).Decode(&user)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
-			return
-		}
-
-		opts := options.Find()
-		filter := bson.M{"user_id": request.User_id}
-		cur, err := roomAdCollection.Find(c, filter, opts)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		var room_ad models.RoomAd
-		for cur.Next(c) {
-			err := cur.Decode(&room_ad)
+			opts := options.Find()
+			filter := bson.M{"user_id": request.User_id}
+			cur, err := roomAdCollection.Find(c, filter, opts)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				return
 			}
-			room_ads = append(room_ads, room_ad)
-		}
 
-		cur_bun, err := bunkieAdCollection.Find(c, filter, opts)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+			var room_ad models.RoomAd
+			for cur.Next(c) {
+				err := cur.Decode(&room_ad)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					return
+				}
+				room_ads = append(room_ads, room_ad)
+			}
 
-		var bunkie_ad models.BunkieAd
-		for cur_bun.Next(c) {
-			err := cur_bun.Decode(&bunkie_ad)
+			cur_bun, err := bunkieAdCollection.Find(c, filter, opts)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				return
 			}
-			bunkie_ads = append(bunkie_ads, bunkie_ad)
-		}
 
-		if user.ProfileInfo != nil {
-			if user.ProfileInfo.DisplayEmail != nil {
-				if !*user.ProfileInfo.DisplayEmail {
+			var bunkie_ad models.BunkieAd
+			for cur_bun.Next(c) {
+				err := cur_bun.Decode(&bunkie_ad)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					return
+				}
+				bunkie_ads = append(bunkie_ads, bunkie_ad)
+			}
+
+			if user.ProfileInfo != nil {
+				if user.ProfileInfo.DisplayEmail != nil {
+					if !*user.ProfileInfo.DisplayEmail {
+						user.Email = nil
+					}
+				} else {
 					user.Email = nil
 				}
-			} else {
-				user.Email = nil
-			}
-			if user.ProfileInfo.DisplayPhone != nil {
-				if !*user.ProfileInfo.DisplayPhone {
+				if user.ProfileInfo.DisplayPhone != nil {
+					if !*user.ProfileInfo.DisplayPhone {
+						user.ProfileInfo.Phone = nil
+					}
+				} else {
 					user.ProfileInfo.Phone = nil
 				}
-			} else {
-				user.ProfileInfo.Phone = nil
 			}
-		}
-		if user.Email == nil {
-			c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
-			return
-		} else {
-			if user.Username == nil {
-				c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
+
+			if user.Email == nil {
+				if user.ProfileInfo.Phone == nil {
+					c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "user_account_info": bson.M{"email": nil, "phone_number": nil}, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
+					return
+				} else {
+					c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "user_account_info": bson.M{"email": nil, "phone_number": *user.ProfileInfo.Phone}, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
+					return
+				}
+			} else {
+				if user.ProfileInfo.Phone == nil {
+					c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "user_account_info": bson.M{"email": *user.Email, "phone_number": nil}, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
+					return
+				} else {
+					c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "user_account_info": bson.M{"email": *user.Email, "phone_number": *user.ProfileInfo.Phone}, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
+					return
+				}
+			}
+
+		} else { // own profile
+			displayBanAndWarn = false
+
+			opts := options.Find()
+			filter := bson.M{"user_id": request.User_id}
+			cur, err := roomAdCollection.Find(c, filter, opts)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				return
-			} else {
-				c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "user_account_info": bson.M{"email": *user.Email, "username": *user.Username}, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
 			}
+
+			var room_ad models.RoomAd
+			for cur.Next(c) {
+				err := cur.Decode(&room_ad)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					return
+				}
+				room_ads = append(room_ads, room_ad)
+			}
+
+			cur_bun, err := bunkieAdCollection.Find(c, filter, opts)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			var bunkie_ad models.BunkieAd
+			for cur_bun.Next(c) {
+				err := cur_bun.Decode(&bunkie_ad)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					return
+				}
+				bunkie_ads = append(bunkie_ads, bunkie_ad)
+			}
+
+			c.JSON(http.StatusOK, gin.H{"user_profile_info": user.ProfileInfo, "user_account_info": bson.M{"email": *user.Email, "username": *user.Username}, "displayBanAndWarn": displayBanAndWarn, "room_ads": room_ads, "bunkie_ads": bunkie_ads})
+
 		}
+
 	}
 }
 
